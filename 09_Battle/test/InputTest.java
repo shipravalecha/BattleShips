99ad704c8a3fefb92dd1520784d2cff2693222ad
akyrimos <akyrimos@uw.edu>
2022-11-12 20:44:05 -0800

    Creating InputTest with Input_Takes_Any_Integer_On_Creation

---
+++ 09_Battle/test/InputTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+
+public class InputTest {
+    Input input;
+
+    // Test to validate Input is created
+    @ParameterizedTest(name = "Input_Takes_Any_Integer_On_Creation")
+    @CsvSource({
+            "-1",
+            "0",
+            "16",
+            "49"
+    })
+    public void Input_Takes_Any_Integer_On_Creation(int val) {
+        input = new Input(val);
+        Assertions.assertNotNull(input);
+    }
+}
