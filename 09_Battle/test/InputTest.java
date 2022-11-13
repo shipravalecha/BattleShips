d1774aff1ac10ef4ff1c368a1d316c266ab041c9
akyrimos <akyrimos@uw.edu>
2022-11-12 22:25:05 -0800

    Added pom.xml, tested importing mockito in InputTest.java

--- 09_Battle/test/InputTest.java
+++ 09_Battle/test/InputTest.java
@@ -2,8 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import java.io.IOException;
+import org.mockito.Mockito.*;
 
 public class InputTest {
     Input input;
