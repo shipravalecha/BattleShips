6ce86594a33f36f9d84df99c0ef77feab6400dcf
troy8chen <troy8chen@gmail.com>
2022-11-21 19:23:25 -0800

    Create parameterized test for id method in Ship.class

---
+++ 09_Battle/test/ShipTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class ShipTest {
+    Ship ship;
+
+    @ParameterizedTest(name = "Id_Function_Valid_Value")
+    @CsvSource({
+            "1, 1",
+            "5, 5",
+            "10, 10"
+    })
+    public void Id_Function_Valid_Value(int id, int expectedId){
+        ship = new Ship(id, 5);
+        int IdActualResult = ship.id();
+        Assertions.assertEquals(IdActualResult, expectedId);
+    }
+
+}
