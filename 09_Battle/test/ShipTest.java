b5e331fc950950ac32839aded98818049fa5be8d
troy8chen <troy8chen@gmail.com>
2022-11-24 15:52:43 -0800

    Adding unit test for wasHit function : where ship was hit on certain coordinate

--- 09_Battle/test/ShipTest.java
+++ 09_Battle/test/ShipTest.java
@@ -133,50 +133,12 @@ public class ShipTest {
 
     }
 
-    // return, exceptions, conditions loop
+    @Test
+    public void test_If_Ship_was_Hit(){
+        ship = new Ship(1,1);
+        ship.place(seaMockForHit,1,1,0);
+        ship.hit(0,1);
+        assertTrue(ship.wasHit(0,1));
+    }
 
-    // For boolean place:
-    // arrange
-    // use fake/mock to call place
-    // 1. -> place --> add sea object with attributes, if placed, or not placed
-
-
-    // 2. cover all switch statements : passing in paramteried tests for all cases
-    // also make sure to catch all exceptions
-
-    // 3. two scenarios: s is empty and s is not empty (passing x and y),
-    // make it return false is good enough
-
-    // 4. if , else in while loop. manipulate what it goes in to if (use mock on
-    // extenship). once it goes to else, two more scenarios
-
-
-
-    // ExtendShip -> all return statements needs to be covered ( test or statement as well)
-    // Create a simple constructor for Sea s, maybe needs to mock for the isEmpty()
-
-    // placeRandom -> test if and exception, initialize a full sea, x and y is not
-    // in consideration anymore.. and it will fail (for 1000 times). -->
-    // or Just use mock to make place return false, then exception would be thrown
-
-    // wasHit -> three conditions if, else, return
-
-    // Hit no return value ???, no worries maybe
-
-
-
-
-
-    //ColorMode colorMock = mock(ColorMode.class);
-    //when(colorMock.getColor(anyInt())).thenReturn(Color.WHITISH);
-    //AdjustableLamp lamp = new AdjustableLamp(colorMock.getColor(anyInt()));
-    //Color expected = Color.DIM;
-    //// Act: refresh twice
-    //        Color actual;
-    //        actual = lamp.refresh();
-    //        actual = lamp.refresh();
-    //
-    //        // Assert
-    //        assertEquals(expected, actual);
-    //        verify(colorMock, times(1)).getColor(anyInt());
 }
